a81ee416e74852c0ac7be3dfb21999047d3a0129
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-09 16:59:36 -0500

    Add linkedList factory function boilerplate

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
 'use strict';
 
+function linkedList() {
+    let head;
+    let tail;
+    let size;
+
+    const append = (value) => {
+        
+    };
+
+    const prepend = (value) => {
+
+    };
+
+    const at = (index) => {
+
+    };
+
+    const pop = () => {
+
+    };
+
+    const contains = (value) => {
+
+    };
+
+    const find = (value) => {
+
+    };
+
+    const toString = () => {
+
+    };
+
+    const insertAt = (value, index) => {
+
+    };
+
+    const removeAt = (index) => {
+
+    };
+
+    return { head, tail, size, append, prepend, at, pop, contains, find, toString, insertAt, removeAt };
+};
+
+let test = linkedList();
+
+console.log(test);
