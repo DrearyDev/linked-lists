d3bfe547a4005583bd39d2c83e6f8d16fdacf308
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-10 16:42:23 -0500

    Select input and submit button

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 import { linkedList } from "./linkedList.js";
 
 console.log(linkedList());
+
+const input = document.querySelector('#list');
+const submitListBtn = document.querySelector('#list-btn');
