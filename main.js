8b7aaccf6c10b6ae7c582329a666dce9a41132e3
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-10 16:44:12 -0500

    Listen for submit button press

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ console.log(linkedList());
 
 const input = document.querySelector('#list');
 const submitListBtn = document.querySelector('#list-btn');
+
+submitListBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    console.log(input.value);
+});
