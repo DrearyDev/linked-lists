208ef27679885fabf008d3983ff93872dc7ca0d2
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-10 16:50:51 -0500

    Add function to validate list passed by user

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ console.log(linkedList());
 const input = document.querySelector('#list');
 const submitListBtn = document.querySelector('#list-btn');
 
+function validateList(inputValue) {
+    inputValue = inputValue.replace(/ /g, '');// remove all spaces
+    inputValue = inputValue.replace(/,,/g, '');// remove two commas in a row
+
+    return inputValue;
+};
+
 submitListBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    console.log(input.value);
+    console.log(validateList(input.value));
+    input.value = '';
 });
