04fdeab39ba8687f911b2d53470d4098e15d93f5
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-10 17:26:33 -0500

    Write function to create linked list

--- main.js
+++ main.js
@@ -5,16 +5,29 @@ console.log(linkedList());
 const input = document.querySelector('#list');
 const submitListBtn = document.querySelector('#list-btn');
 
+let linked;
+
+function createLinkedList(listarr) {
+    linked = linkedList();
+
+    for (let i in listarr) {
+        linked.append(listarr[i]);
+    };
+
+    console.log(linked.toString());
+};
+
 function validateList(inputValue) {
     inputValue = inputValue.replace(/ /g, '');// remove all spaces
     inputValue = inputValue.replace(/,,/g, '');// remove two commas in a row
 
-    return inputValue;
+    return inputValue.split(',');
 };
 
 submitListBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    console.log(validateList(input.value));
+    let listArr = validateList(input.value);
+    createLinkedList(listArr);
     input.value = '';
 });
