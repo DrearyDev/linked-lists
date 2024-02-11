33e48a6df4523bf29bcf4d1f8d0c3cf7c553e6b5
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-10 23:14:16 -0500

    Add insertAt and removeAt method functionality

--- main.js
+++ main.js
@@ -59,6 +59,52 @@ function validateMethodInput(method, input) {
         find: function(input) {
             if (linked.find(input) === null) { info.innerText = 'not found' }
             else { info.innerText = `found at index: ${linked.find(input)}` };
+        },
+        insertAt: function(input) {
+            let value, index;
+
+            if (input.includes(',')) {
+                [value, index] = input.split(',');
+
+                index = Number(index);
+
+                if (index < 0 || index > linked.getSize()) {
+                    methodUserInput.setCustomValidity('Index is out of bounds');
+                    info.innerText = methodUserInput.validationMessage;
+                    methodUserInput.style.border = '1px solid red';
+                } else {
+                    methodUserInput.setCustomValidity('');
+                    methodUserInput.style.border = '1px solid black';
+
+                    info.innerText = `inserted: ${value} at index ${index}`;
+
+                    linked.insertAt(value, index);
+                    stringOutput.innerText = linked.toString();
+                };
+
+            } else {
+                methodUserInput.setCustomValidity('input must be in format: value,index');
+                info.innerText = methodUserInput.validationMessage;
+                methodUserInput.style.border = '1px solid red';
+            };
+
+        },
+        removeAt: function(input) {
+            input = Number(input);
+
+            if (input < 0 || input > linked.getSize() - 1) {
+                methodUserInput.setCustomValidity('Index is out of bounds');
+                info.innerText = methodUserInput.validationMessage;
+                methodUserInput.style.border = '1px solid red';
+            } else {
+                methodUserInput.setCustomValidity('');
+                methodUserInput.style.border = '1px solid black';
+
+                info.innerText = 'removed: ' + linked.at(input).value;
+
+                linked.removeAt(input);
+                stringOutput.innerText = linked.toString();
+            };
         }
     }[method] ?? function(input) {
         linked[method](input);
